859355341fad3cf7c9bad6d4585ca780ee7e5edc
actionanand <meetanandns@gmail.com>
2020-06-17 23:00:14 +0530

    d3 data binding and join

--- public/app.js
+++ public/app.js
@@ -14,7 +14,38 @@ const countryData = {
 
 d3.select('ul')
 .selectAll('li')
-.data(countryData.items)
+.data(countryData.items, data => data)
 .enter()
 .append('li')
 .text(data => data);
+
+setTimeout( () => {
+    countryData.addItem('China');
+    d3.select('ul')
+    .selectAll('li')
+    .data(countryData.items, data => data)
+    .enter()
+    .append('li')
+    .classed('added', true)
+    .text(data => data);
+}, 2000);
+
+
+setTimeout( () => {
+    countryData.removeItem(0);
+    d3.select('ul')
+    .selectAll('li')
+    .data(countryData.items, data => data)
+    .exit()
+    .classed('redundant', true);
+}, 4000);
+
+setTimeout( () => {
+    countryData.updateItem(1, 'Canada');
+    d3.select('ul')
+    .selectAll('li')
+    .data(countryData.items, data => data)
+    .exit()
+    .classed('updated', true)
+    .text('Canada');
+}, 6000);
